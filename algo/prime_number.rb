81d733846b5df48f56bc1371c42607c2dec63718
Dov Bluming <dovybluming@gmail.com>
2017-04-02 01:50:04 -0400

    weekend work 5

--- algo/prime_number.rb
+++ algo/prime_number.rb
@@ -3,9 +3,15 @@
 # First, implement the is_prime? method below. It should accept a number as an argument and return
 # true if the number is prime and false if it is not. A prime number is a number that is only
 # divisible by itself and the number 1.
-
 def is_prime?(number)
-
+  factors = []
+  (2..(Math.sqrt(number))).each do |divisible_by|
+    if number % divisible_by == 0
+      factors << divisible_by
+      factors << (number / divisible_by)
+    end
+  end
+  factors == [] ? true : false
 end
 
 # Second, implement the highest_prime_number_under method below. It should accept a number as
@@ -13,5 +19,15 @@ end
 # highest prime number under 10 is 7.
 
 def highest_prime_number_under(number)
-
+  prime_numbers = []
+  (2..number).each do |num|
+    if is_prime?(num)
+      prime_numbers << num
+    end
+  end
+  
+  return prime_numbers.max
 end
+p is_prime?(7)
+p highest_prime_number_under(200)
+  
--- oop/thesaurus.rb
+++ oop/thesaurus.rb
@@ -10,3 +10,57 @@
 #
 # Part of the challenge is to determine which functionality belongs in the Thesaurus class,
 # and which belongs in the Entry class.
+class Thesaurus
+  attr_reader :words
+  def add_word(word, synonym, antonym)
+    if !@words
+      @words = {}
+    end
+    entry = Entry.new(word, synonym, antonym)
+    @words[entry.word] = entry
+  end
+
+  def delete_word(word)
+    @words.delete(word)
+  end
+ 
+end
+
+class Entry
+  attr_reader :word, :synonyms, :antonyms
+  def initialize(word, synonym, antonym)
+    @word = word
+    @synonyms = [] << synonym
+    @antonyms = [] << antonym
+  end
+  
+  def add_synonym(synonym)
+    synonyms << synonym
+  end
+
+  def add_antonym(antonym)
+    antonyms << antonym
+  end
+end
+
+thesaurus = Thesaurus.new
+# add words
+thesaurus.add_word("good", "great", "bad")
+thesaurus.add_word("man", "guy", "blb")
+p thesaurus.words
+p "---------"
+# delete word
+thesaurus.delete_word("man")
+p thesaurus.words
+p "---------"
+# look up synonyms and antonyms
+p thesaurus.words["good"].synonyms
+p thesaurus.words["good"].antonyms
+p "---------"
+# add synonym
+thesaurus.words["good"].add_synonym("excellent")
+p thesaurus.words["good"].synonyms
+p "---------"
+# add antonym
+thesaurus.words["good"].add_antonym("poor")
+p thesaurus.words["good"].antonyms
